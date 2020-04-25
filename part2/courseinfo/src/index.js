63e0086be31f3e7dcf481e86ac5d8a714e12fa05
Rudresh Raval <rudresh.r@somaiya.edu>
2020-04-26 00:01:24 +0530

    2.1 done

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -34,30 +34,36 @@ const Content = ({ course }) => {
 
 const App = () => {
   const course = {
-    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
+        exercises: 10
       },
       {
         name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
+        exercises: 7
       },
       {
         name: 'State of a component',
-        exercises: 14,
-        id: 3
+        exercises: 14
       }
     ]
+  }
+  const Course = ({course}) => {
+    return(
+    <>
+    <Header course ={course}></Header>
+    <Content course={course}></Content>
+    </>
+    )
+
+
   }
 
   return (
     <div>
-      <Course course={course} />
+      <Course course={course}></Course>
     </div>
   )
 }
